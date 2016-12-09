77fa03f9ded4c730595947b6a1ac657becec23d8
BurnOutDev <BurnOutDev@BURNOUTDEV>
2016-12-09 16:02:04 +0400

    Add project files.

---
+++ BePro.Website.Public/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BePro.Website.Public.MvcApplication" Language="C#" %>
---
+++ BePro.Website.Public/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BePro.Website.Public.Startup))]
+namespace BePro.Website.Public
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
