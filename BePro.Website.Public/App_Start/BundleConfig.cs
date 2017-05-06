1970687e05dda65acc230f46cd2174fd9f603091
BurnOutDev <iraklimurusidze@live.com>
2017-05-07 03:42:23 +0400

    Added BundleConfig (not finished)

---
+++ BePro.Website.Public/App_Start/BundleConfig.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Optimization;
+
+namespace BePro.Website.Public
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/css/style.css").Include(
+                "~/Content/Css/bootstrap.css",
+                "~/Content/Css/prettyPhoto.css",
+                "~/Content/Css/Site.css",
+                "~/Content/Css/fonts/font-awesome.css",
+                "~/Content/Css/fonts/bpg-mrgvlovani.min.css",
+                "~/Content/Css/fonts/bpg-mrgvlovani-caps.min.css",
+                "~/Content/Css/fonts/OpenSans.css",
+                "~/Content/Css/Lobibox/lobibox.css"
+            ));
+
+            bundles.Add(new ScriptBundle("~/js/main.js").Include(
+                "~/Content/Scripts/TweenMax.min.js",
+                "~/Content/Scripts/modernizr-2.8.3.js",
+                "~/Content/Scripts/jquery-3.1.1.js",
+                "~/Content/Scripts/bootstrap.js",
+                "~/Content/Scripts/respond.js",
+                "~/Content/Scripts/jquery.prettyPhoto.js",
+                "~/Content/Scripts/jquery.parallax.js",
+                "~/Content/Scripts/jquery.isotope.js",
+                "~/Content/Scripts/main.js",
+                "~/Content/Scripts/lobibox/lobibox.js"
+            ));
+        }
+    }
+}
--- BePro.Website.Public/Global.asax.cs
+++ BePro.Website.Public/Global.asax.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -15,6 +11,7 @@ namespace BePro.Website.Public
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
